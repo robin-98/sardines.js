66fc6df5e655cfa5edd0c0556e33cf9e53dd905b
Robin <robin@naturewake.com>
2019-07-26 03:58:26 +0800

    prepare to build

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,51 @@
-@sealed
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
+/**
+ * @author Robin Sun
+ * @email robin@naturewake.com
+ * @create date 2019-07-26 03:10:17
+ * @modify date 2019-07-26 03:10:17
+ * @desc [description]
+ */
+import * as utils from 'sardines-utils'
+
+export interface RepositoryEntry {
+    address: any
+    driver: any
 }
 
-function sealed(constructor: Function) {
-    Object.seal(constructor);
-    Object.seal(constructor.prototype);
-    console.log(Object.seal(constructor.prototype), Object.seal(constructor))
+let entries: RepositoryEntry[] = []
+
+export const requestRepoService = async (moduleName: string, service: string, ...args:any[]) => {
+    const badEntries: RepositoryEntry[] = []
+    for (let i = entries.length-1; i>=0; i--) {
+        const entry = entries[i]
+        try {
+            const driverInstance = utils.Factory.getInstance(entry.driver, entry.address)
+            if (driverInstance && driverInstance.invoke) {
+                return await driverInstance.invoke({application: null, module: moduleName, service}, ...args)
+            }
+        } catch (e) {
+            badEntries.push(entries.pop()!)
+        }
+    }
+    Array.prototype.unshift.apply(entries, badEntries)
+    return null
+}
+export const setup = (entries: RepositoryEntry[]) => {
+    entries = entries.reverse()
 }
 
-// sealed(Greeter);
+export const isRemote = (application: string, moduleName: string, service: string):boolean => {
+    return (application === '' && moduleName === '' && service === '')
+}
+
+export interface InvokeParameters {
+    application: string
+    module: string
+    service: string
+ }
+export const invoke = (params: InvokeParameters, ...args: any[]) => {
+    return params && args
+}
+
+
+
--- src/school.ts
+++
@@ -1,6 +0,0 @@
-
-export class School {
-    constructor() {
-        
-    }
-}
