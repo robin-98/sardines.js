8d6ce6532109020d626be09a382993a7dc74e14a
Robin <robin@naturewake.com>
2019-07-27 18:17:52 +0800

    fixed bugs of proxy code generator

--- src/index.ts
+++ src/index.ts
@@ -5,42 +5,7 @@
  * @modify date 2019-07-26 03:10:17
  * @desc [description]
  */
-import * as utils from 'sardines-utils'
 
-export interface RepositoryEntry {
-    address: any
-    driver: any
-}
-
-let entries: RepositoryEntry[] = []
-
-export const requestRepoService = async (moduleName: string, service: string, ...args:any[]) => {
-    const badEntries: RepositoryEntry[] = []
-    let res = null
-    for (let i = entries.length-1; i>=0; i--) {
-        const entry = entries[i]
-        try {
-            const driverInstance = utils.Factory.getInstance(entry.driver, entry.address)
-            if (driverInstance && driverInstance.invoke) {
-                res = await driverInstance.invoke({application: null, module: moduleName, service}, ...args)
-                break
-            } else {
-                throw 'bad driver'
-            }
-        } catch (e) {
-            if (e !== 'bad driver' && e.message !== 'bad driver') {
-                badEntries.push(entries.pop()!)
-            } else {
-                entries.pop()
-            }
-        }
-    }
-    Array.prototype.unshift.apply(entries, badEntries)
-    return res 
-}
-export const setup = (entries: RepositoryEntry[]) => {
-    entries = entries.reverse()
-}
 
 export const isRemote = (application: string, moduleName: string, service: string):boolean => {
     return (application === '' && moduleName === '' && service === '')
@@ -51,9 +16,11 @@ export interface InvokeParameters {
     module: string
     service: string
  }
-export const invoke = (params: InvokeParameters, ...args: any[]) => {
+export const invoke = async (params: InvokeParameters, ...args: any[]) => {
     return params && args
 }
 
+import * as Repository from './repository'
+export { Repository }
 
 
