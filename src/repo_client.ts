d56522c4bef69074b8287e1227d695946b5ac79e
Robin <robin@naturewake.com>
2019-12-12 16:44:47 +0800

    sardine project automatic save point

--- src/repo_client.ts
+++ src/repo_client.ts
@@ -180,9 +180,7 @@ export namespace RepositoryClient {
           let customArgs: any[] = <any[]>customArguments(ArgumentType.args, entry, service, ...args)
           try {
             serviceDefinition.application = repoAppName
-            console.log('going to invoke driver [' + pvdr.driver + '] for service:', serviceDefinition, ', driverInst:', driverInst, ', provider:', pvdr)
             res = await driverInst.invokeService(Sardines.Transform.fromServiceToEmptyRuntime(serviceDefinition)!, ...customArgs)
-            console.log('driver invoked for service [', serviceDefinition,'], response:', res)
             switch (serviceDefinition.returnType) {
               case 'string': case 'number': case 'boolean':
                 break
